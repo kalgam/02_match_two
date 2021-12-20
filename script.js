215e45941d41cd328c8571fd3bd0d9049c728396
kalgam <96410135+kalgam@users.noreply.github.com>
2021-12-20 15:02:39 -0800

    test

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+var bg = []
+
 function appInit() {
-   
+    bg.push(14, 14);
+    bg.push(15, 14);
+    bg.push(15, 14);
+    bg.push(15, 15);
 }
 
 function appUpdate(dt) {
@@ -8,6 +13,11 @@ function appUpdate(dt) {
     }
 }
 
+// drawSprite(x, y, tileX, tileY)
+// drawSprite(x, y, tileX, tileY, spriteWidth, wpriteHeight)
+// drawMap(x, y, [tileX, tileY], coloms, rows)
 function appDraw() {
     drawSprite(0, 0, 0, 0, 16, 16);
+
+    drawMap(100, 100, bg, 2, 2);
 }
